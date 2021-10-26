973f240032a46a5724d6fa75c6d1cce85de0c8db
DZ <62887535+sunscity@users.noreply.github.com>
2021-10-26 15:08:28 +0300

    Added Test Building

---
+++ src/Tests/Test_Building.java
@@ -0,0 +1,43 @@
+package Tests;
+
+import ex0.Building;
+import ex0.Elevator;
+
+public class Test_Building implements Building {
+    String name;
+    int min,max,numOfElevators;
+    Elevators []elevators;
+
+    public Test_Building (String name,int min,int max,int numOfElevators){
+        this.name=name;
+        this.min=min;
+        this.max=max;
+        this.numOfElevators=numOfElevators;
+        Elevators []arr=new Elevator [numOfElevators]();
+    }
+
+    @Override
+    public String getBuildingName() {
+        return this.name;
+    }
+
+    @Override
+    public int minFloor() {
+        return this.min;
+    }
+
+    @Override
+    public int maxFloor() {
+        return this.max;
+    }
+
+    @Override
+    public int numberOfElevetors() {
+        return this.numOfElevators;
+    }
+
+    @Override
+    public Elevator getElevetor(int i) {
+        return Elevators[i];
+    }
+}
